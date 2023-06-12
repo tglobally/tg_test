6c77da82a2b7fd32f0dfb85bc8e750592d7e0bee
Kevin <kevin.acuna@ivitec.com.mx>
2023-06-12 16:43:52 -0500

    v1.0.0 Se inicializa test

---
+++ src/liberator.php
@@ -0,0 +1,4 @@
+<?php
+namespace tglobally\test;
+
+class liberator extends \gamboamartin\test\liberator {}
---
+++ src/test.php
@@ -0,0 +1,4 @@
+<?php
+namespace tglobally\test;
+
+class test extends \gamboamartin\test\test {}
